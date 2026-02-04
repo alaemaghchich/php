134e241956167cf514dcee81ee4e79ae5d22bacf
alaemaghchich <alaemaghchich2004@gmail.com>
2026-02-04 10:34:23 +0000

    PHP

--- Bases et logique PHP/prototype & realissation/prototype/calcul.php
+++ Bases et logique PHP/prototype & realissation/prototype/calcul.php
@@ -1,10 +1,19 @@
 <?php
 
-function avg($notes) {
-    return array_sum($notes) / count($notes);
-}
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
-function getMention($moyenne) {
+    $nom   = $_POST['nom'];
+    $note1 = $_POST['note1'];
+    $note2 = $_POST['note2'];
+    $note3 = $_POST['note3'];
+
+    $notes = [$note1, $note2, $note3];
+
+    function avg($notes) {
+        return array_sum($notes) / count($notes);
+    }
+
+  function getMention($moyenne) {
     if ($moyenne >= 16) {
         return "Very Good";
     } elseif ($moyenne >= 14) {
@@ -15,38 +24,46 @@ function getMention($moyenne) {
         return "Pass";
     } else {
         return "fail";
+    }}
+
+  if($nom === "" || $note1 === "" || $note2 === "" || $note3 === "") {
+    echo "All inputs are required";
+    exit;
+}
+
+
+    if (min($notes) >= 0 && max($notes) <= 20) {
+        $moyenne = avg($notes);
+        $mention = getMention($moyenne);
+
+        echo "<div>
+            Student: $nom <br>
+            Average: $moyenne / 20 <br>
+            Montion: $mention
+        </div>";
+    } else {
+        echo "Error: grades must be between 0 and 20";
     }
 }
-
-$resultat = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nom   = $_POST['nom'];
-    $note1 = $_POST['note1'];
-    $note2 = $_POST['note2'];
-    $note3 = $_POST['note3'];
-}
-
-$notes = [$note1, $note2, $note3];
-
-$valid = true;
-foreach ($notes as $note) {
-    if ($note < 0 || $note > 20) {
-        $valid = false;
-        break;
+?>
+<style>
+    body{
+        background-image: url(school-education-seamless-pattern-vector-45436158.avif);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
     }
-}
+    div{
+        background-color: lightgray;
+        height: 250px;
+        width: 400px;
+        border-radius: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        line-height: 50px;
+        font-size: x-large;
 
-if ($valid) {
-    $moyenne = avg($notes);
-    $mention = getMention($moyenne);
-    $resultat = "Student: $nom <br> Average: $moyenne / 20 <br> Mention: $mention";
-} else {
-    $resultat = "Error: The grades must be between 0 and 20.";
-}
-
-if ($resultat != "") {
-    echo "<div>$resultat</div>";
-} else {
-    echo "No data received.";
-}
+    }
+</style>
